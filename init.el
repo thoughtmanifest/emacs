4b9efce4f52a63e8c7e69e0afd53d69df870f8ae
David Sison <daviddsison@gmail.com>
2016-08-09 16:12:38 -0400

    Working inf-clojure config

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; melpa package management
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
@@ -5,3 +6,155 @@
    '("melpa" . "http://melpa.org/packages/")
    t)
   (package-initialize))
+;; refresh contents if necessary
+(when (not package-archive-contents)
+  (package-refresh-contents))
+;;
+
+;; so it can find lein and other commands
+(add-to-list 'exec-path "/usr/local/bin")
+;;
+
+;; list packages
+(defvar my-packages
+  '(;; makes handling lisp expressions much, much easier
+    ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
+    paredit
+
+    ;; key bindings and code colorization for Clojure
+    ;; https://github.com/clojure-emacs/clojure-mode
+    clojure-mode
+
+    ;; extra syntax highlighting for clojure
+    clojure-mode-extra-font-locking
+
+    ;; inf-clojure
+    inf-clojure
+        
+    ;; org-mode
+    org
+
+    ;; allow ido usage in as many contexts as possible. see
+    ;; customizations/navigation.el line 23 for a description
+    ;; of ido
+    ido-ubiquitous
+
+    ;; Enhances M-x to allow easier execution of commands. Provides
+    ;; a filterable list of possible commands in the minibuffer
+    ;; http://www.emacswiki.org/emacs/Smex
+    smex
+
+    ;; project navigation
+    projectile
+
+    ;; colorful parenthesis matching
+    rainbow-delimiters
+
+    ;; edit html tags like sexps
+    tagedit
+
+    ;; git integration
+    magit))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+;;
+
+;; inf-clojure
+(autoload 'inf-clojure "inf-clojure" "Run an inferior Clojure process" t)
+(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+;;
+
+;; ElDoc
+(add-hook 'clojure-mode-hook #'eldoc-mode)
+(add-hook 'inf-clojure-mode-hook #'eldoc-mode)
+;;
+
+;; undo-tree
+(require 'undo-tree)
+(global-undo-tree-mode)
+;;
+
+;; rainbow delimiters mode
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'inf-clojure-mode-hook #'rainbow-delimiters-mode)
+;; 
+
+;; paredit
+(require 'paredit)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'inf-clojure-mode-hook #'paredit-mode)
+
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code."
+  t)
+(add-hook 'lisp-mode-hook '(lambda () (paredit-mode +1)))
+(add-hook 'emacs-lisp-mode-hook '(lambda () (paredit-mode +1)))
+
+(show-paren-mode t)
+;;
+
+;; ido mode 
+(ido-mode 1)
+(ido-everywhere 1)
+;; ido-ubiquitous
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+;;
+
+;; smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;;
+
+;; projectile
+(projectile-global-mode)
+;;
+
+;; magit https://magit.vc/manual/magit/Getting-started.html#Getting-started
+(global-set-key (kbd "C-x g") 'magit-status)
+;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (cyberpunk)))
+ '(custom-safe-themes
+   (quote
+    ("cdbd0a803de328a4986659d799659939d13ec01da1f482d838b68038c1bb35e8" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "7c33d91f9896614a9c28e96def4cbd818f0aa7f151d1fb5d205862e86f2a3939" default)))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#202020")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#C99090")
+     (40 . "#D9A0A0")
+     (60 . "#ECBC9C")
+     (80 . "#DDCC9C")
+     (100 . "#EDDCAC")
+     (120 . "#FDECBC")
+     (140 . "#6C8C6C")
+     (160 . "#8CAC8C")
+     (180 . "#9CBF9C")
+     (200 . "#ACD2AC")
+     (220 . "#BCE5BC")
+     (240 . "#CCF8CC")
+     (260 . "#A0EDF0")
+     (280 . "#79ADB0")
+     (300 . "#89C5C8")
+     (320 . "#99DDE0")
+     (340 . "#9CC7FB")
+     (360 . "#E090C7"))))
+ '(vc-annotate-very-old-color "#E090C7"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
