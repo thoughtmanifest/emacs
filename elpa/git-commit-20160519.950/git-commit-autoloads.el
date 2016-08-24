d1628e36b4355f465f13bbc04cd57ae88a209934
David Sison <daviddsison@gmail.com>
2016-08-24 13:42:22 -0400

    Add all loaded els for easy clone to new machine

---
+++ elpa/git-commit-20160519.950/git-commit-autoloads.el
@@ -0,0 +1,15 @@
+;;; git-commit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("git-commit.el") (22442 8895 648721 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-commit-autoloads.el ends here
---
+++ elpa/zerodark-theme-20160803.1408/zerodark-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zerodark-theme" "20160803.1408" "A dark, medium contrast theme for Emacs" 'nil :url "https://github.com/NicolasPetton/zerodark-theme" :keywords '("themes"))
