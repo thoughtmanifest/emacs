accbceba984c3b274fcd32bf9a687c9ccc9ec840
David Sison <david@metismachine.com>
2018-07-20 10:46:50 -0400

    Update all the things

---
+++ elpa/dumb-jump-0.5.1/dumb-jump-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dumb-jump" "0.5.1" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.17.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :commit "64c0759432334465ccea115d83600b468f86e3be" :keywords '("programming"))
--- elpa/inf-clojure-1.4.0/inf-clojure-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "inf-clojure" "1.4.0" "Run an external Clojure process in an Emacs buffer" '((emacs "24.1") (clojure-mode "5.0")) :url "http://github.com/clojure-emacs/inf-clojure" :keywords '("processes" "clojure"))
