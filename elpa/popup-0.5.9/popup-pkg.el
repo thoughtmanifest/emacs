a74eb086f6caf3e32fcefad04cd8198abbdbe852
David Sison <thoughtmanifest@users.noreply.github.com>
2025-11-23 08:04:37 -0500

    Updated to latest

---
+++ elpa/popup-0.5.9/popup-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "popup" "0.5.9"
+  "Visual Popup User Interface."
+  '((emacs "24.3"))
+  :url "https://github.com/auto-complete/popup-el"
+  :commit "20ce6cbd2f06423be35b3b700c698f0e109e880c"
+  :revdesc "20ce6cbd2f06"
+  :keywords '("lisp")
+  :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
+  :maintainers '(("Jen-Chieh" . "jcs090218@gmail.com")))
--- elpa/with-editor-20160828.1025/with-editor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; with-editor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22473 33460 318073 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; with-editor-autoloads.el ends here
